42c0b6467ba675dfd1f4e6b0c6641a09c9e21e9c
wangxiechen <52745621+wangxiechen@users.noreply.github.com>
2019-07-11 08:21:07 +0800

    Update User.java

--- wangxiechen_demo5/src/main/java/com/wangxiechen/demo5/User.java
+++ wangxiechen_demo5/src/main/java/com/wangxiechen/demo5/User.java
@@ -3,5 +3,7 @@ package com.wangxiechen.demo5;
 public class User {
 	public static void main(String[] args) {
 		System.out.println("helloWord");
+		System.out.println("helloWord");
+		System.out.println("helloWord");
 	}
 }
